a50f9fc79b01fd2c0d2de5328dcdb38ea0b475a3
MdNahid360 <mdnahid360s@gmail.com>
2021-03-18 12:29:25 -0700

    frist commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDmNdgCT-6QR9cIfVZGFiBRLNX4I-OQdZo",
+    authDomain: "bdrider-ac5bd.firebaseapp.com",
+    projectId: "bdrider-ac5bd",
+    storageBucket: "bdrider-ac5bd.appspot.com",
+    messagingSenderId: "200738670753",
+    appId: "1:200738670753:web:20bc01bad23e0ed57848e0"
+  };
+
+  export default firebaseConfig;
